bdc255fb12c383eb35c0323ba3c962c1d8105b4a
anantachi ittiworapong <anantachai@idea2mobile.com>
2019-01-04 08:29:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <!-- This file allows you to host this page as a static file on Heroku -->
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php header( 'Location: /https://idea2mobile.com/mobile/fleet2.php' ) ;  ?>
